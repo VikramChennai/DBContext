c06812740622ccf30ebf35164eefafc93b26ce8d
Ishan Paidhungat <ishanpaidhungat@gmail.com>
2024-09-08 10:34:42 -0700

    fix code

--- PostgreSQL/create_test_tables.py
+++ PostgreSQL/create_test_tables.py
@@ -1,38 +1,36 @@
-import psycopg2
-from psycopg2 import sql
 import argparse
+import asyncpg
+import asyncio
 
-def connect_to_db(username, password, database, host, port):
+
+async def connect_to_db(username, password, dbname, host, port):
     try:
-        conn = psycopg2.connect(
-            dbname=database,
-            user=username,
-            password=password,
-            host=host,
-            port=port
-        )
+        conn = await asyncpg.connect(
+                user=username,
+                password=password,
+                database=dbname,
+                host=host,
+                port=port
+            )
         print("Connected to the database successfully.")
         return conn
-    except psycopg2.Error as e:
+    except Exception as e:
         print(f"Unable to connect to the database: {e}")
         return None
 
-def execute_sql_file(conn, file_path):
+async def execute_sql_file(conn, file_path):
     try:
         with open(file_path, 'r') as file:
             sql_commands = file.read()
 
-        with conn.cursor() as cur:
-            cur.execute(sql_commands)
-        conn.commit()
+        await conn.execute(sql_commands)
         print("SQL commands executed successfully.")
-    except psycopg2.Error as e:
+    except asyncpg.PostgresError as e:
         print(f"Error executing SQL commands: {e}")
-        conn.rollback()
     except IOError as e:
         print(f"Error reading SQL file: {e}")
 
-def main():
+async def main():
     parser = argparse.ArgumentParser(description="Set up database tables and import data.")
     parser.add_argument("--username", required=True, help="Database username")
     parser.add_argument("--password", required=True, help="Database password")
@@ -43,11 +41,11 @@ def main():
 
     args = parser.parse_args()
 
-    conn = connect_to_db(args.username, args.password, args.database, args.host, args.port)
+    conn = await connect_to_db(args.username, args.password, args.database, args.host, args.port)
     if conn:
-        execute_sql_file(conn, args.sql_file)
-        conn.close()
+        await execute_sql_file(conn, args.sql_file)
+        await conn.close()
         print("Database connection closed.")
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
